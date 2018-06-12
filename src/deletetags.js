f7b08c10c5729b16464bfc30e50a59aadfd3c709
piligab <pilar@developmentseed.org>
2018-06-12 18:17:36 -0500

    Update files

--- src/deletetags.js
+++ src/deletetags.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 //var argv = require('minimist')(process.argv.slice(2));
 
-module.exports = function (file) {
+//module.exports = function (file) {
 
 var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
 
