a272f9b81f7bbd962af6ac13076c0f84c9b4c7f4
piligab <pilar@developmentseed.org>
2018-06-12 18:36:46 -0500

    Update files

--- src/deletetags.js
+++ src/deletetags.js
@@ -2,28 +2,28 @@ var fs = require('fs');
 //var argv = require('minimist')(process.argv.slice(2));
 const turf = require('@turf/turf');
 
-module.exports = function (file) {
+module.exports = function(file) {
 
-//var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
-var obj = JSON.parse(fs.readFileSync(file).toString());
+  //var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
+  var obj = JSON.parse(fs.readFileSync(file).toString());
 
 
-for (var i = 0; i < obj.features.length; i++) {
+  for (var i = 0; i < obj.features.length; i++) {
 
-  delete obj.features[i].properties.timestamp;
-  delete obj.features[i].properties.version;
-  delete obj.features[i].properties.changeset;
-  delete obj.features[i].properties.user;
-  delete obj.features[i].properties.uid;
-  delete obj.features[i].properties.uid;
-  delete obj.features[i].properties.id;
-}
+    delete obj.features[i].properties.timestamp;
+    delete obj.features[i].properties.version;
+    delete obj.features[i].properties.changeset;
+    delete obj.features[i].properties.user;
+    delete obj.features[i].properties.uid;
+    delete obj.features[i].properties.uid;
+    delete obj.features[i].properties.id;
+  }
 
-fs.writeFile("output.geojson", JSON.stringify(obj) , function(err) {
-    if(err) {
-        return console.log(err);
+  fs.writeFile("output.geojson", JSON.stringify(obj), function(err) {
+    if (err) {
+      return console.log(err);
     }
     console.log("The file was saved!");
-});
+  });
 
 };
